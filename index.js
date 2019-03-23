1db3fbf85b08fde5b442fb8425b985fe9821ac60
jy <nurja1218@gmail.com>
2019-03-23 13:46:03 +0900

    port4000 호출시 get으로 req,res 보내고 받아오는 function 추가

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hi');
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+    // console.log('Listening on: http://localhost:' + PORT);
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+    console.log(req);
+    res.send('Hello from Home!');
+}
+
+function handleProfile(req, res) {
+    res.send('You are on my Profile');
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
