d36c9df7650863d7edf2c8fa79ef3d0e0a6560c7
jy <nurja1218@gmail.com>
2019-03-23 14:43:37 +0900

    index.js babel 문법 update, .babelrc create

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const express = require('express');
+// const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
+// function handleListening() {
+//     // console.log('Listening on: http://localhost:' + PORT);
+//     console.log(`Listening on: http://localhost:${PORT}`);
+// }
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleListening() {
-    // console.log('Listening on: http://localhost:' + PORT);
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+// function handleHome(req, res) {
+//     console.log(req);
+//     res.send('Hello from Home!');
+// }
+const handleHome = (req, res) => res.send('Hello from Guri');
 
-function handleHome(req, res) {
-    console.log(req);
-    res.send('Hello from Home!');
-}
+// function handleProfile(req, res) {
+//     res.send('You are on my Profile');
+// }
+const handleProfile = (req, res) => res.send('You are on my Profile');
 
-function handleProfile(req, res) {
-    res.send('You are on my Profile');
-}
 
 app.get("/", handleHome);
 
