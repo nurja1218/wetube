6290c1226951861aa8775008b440ce1cd7aac2c6
jy <nurja1218@gmail.com>
2019-03-26 19:31:30 +0900

    user,video,global 각 url을 routes.js를 사용하여 설정 & pug파일로 '/home' 설정

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
+import { users, userDetail, editProfile, changePassword } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send("Users"));
-userRouter.get(routes.userDetail, (req, res) => res.send("UserDetail"));
-userRouter.get(routes.editProfile, (req, res) => res.send("EditProfile"));
-userRouter.get(routes.changePassword, (req, res) => res.send("ChangePassword"));
+userRouter.get(routes.users, (req, res) => users);
+userRouter.get(routes.userDetail, (req, res) => userDetail);
+userRouter.get(routes.editProfile, (req, res) => editProfile);
+userRouter.get(routes.changePassword, (req, res) => changePassword);
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import routes from "../routes"
+import { videos, upload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, (req, res) => res.send("Videos"));
-videoRouter.get(routes.upload, (req, res) => res.send("Upload"));
-videoRouter.get(routes.videoDetail, (req, res) => res.send("VideoDetail"));
-videoRouter.get(routes.editVideo, (req, res) => res.send("EditVideo"));
-videoRouter.get(routes.deleteVideo, (req, res) => res.send("DeleteVideo"));
+videoRouter.get(routes.videos, (req, res) => videos);
+videoRouter.get(routes.upload, (req, res) => upload);
+videoRouter.get(routes.videoDetail, (req, res) => videoDetail);
+videoRouter.get(routes.editVideo, (req, res) => editVideo);
+videoRouter.get(routes.deleteVideo, (req, res) => deleteVideo);
 
 export default videoRouter;
