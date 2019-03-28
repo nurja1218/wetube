ccb5c21ac8b40faef279f860d028f2cfaa93b2d0
jy <nurja1218@gmail.com>
2019-03-28 15:58:55 +0900

    컨트롤러의 search뷰의 render를 input으로 받은 data를 가져와 사용하도록 수정

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,18 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" , name: "wonderful site"});
-export const search = (req, res) => res.render("search", { pageTitle: "Search" });
+
+export const search = (req, res) => {
+    const {
+        query: { clue: outcome }
+    } = req;
+    res.render("search", { pageTitle: "Search", outcome});
+};
+
 export const videos = (req, res) => res.render("videos", { pageTitle: "Videos" });
+
 export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
