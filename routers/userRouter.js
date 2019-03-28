ba11cdb1e94ce15576432c88fc603741c4915a3f
jy <nurja1218@gmail.com>
2019-03-28 18:37:35 +0900

    .../users/change-password로 이동하도록 userRouter.get 순서 변경

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,7 +5,7 @@ import { users, userDetail, editProfile, changePassword } from "../controllers/u
 const userRouter = express.Router();
 
 userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
