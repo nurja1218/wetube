6b5f6f7c47507814bfdefc556717556c177b82f1
jy <nurja1218@gmail.com>
2019-03-29 18:11:18 +0900

    가상데이터 db.js에 videos(배열) 생성

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 12345,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Jun",
+            email: "nurja1218@gmail.com"
+        }
+    },
+    {
+        id: 333333,
+        title: 'Video super',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Jun",
+            email: "nurja1218@gmail.com"
+        }
+    },
+    {
+        id: 444444,
+        title: 'Video power',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Jun",
+            email: "nurja1218@gmail.com"
+        }
+    },
+    {
+        id: 555555,
+        title: 'Video cool',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Jun",
+            email: "nurja1218@gmail.com"
+        }
+    }
+]
