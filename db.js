fb9adb6dcd7929d1e7f9adb41cd8e6eeb1e86fe4
jy <nurja1218@gmail.com>
2019-03-29 18:55:27 +0900

    videoFile의 주소 링크 수정

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
         title: 'Video awesome',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Jun",
@@ -16,7 +16,7 @@ export const videos = [
         title: 'Video super',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Jun",
@@ -28,7 +28,7 @@ export const videos = [
         title: 'Video power',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Jun",
@@ -40,7 +40,7 @@ export const videos = [
         title: 'Video cool',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Jun",
