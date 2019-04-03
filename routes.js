ad1a3f7e46a1d1f155edbac164b2a3144bed2d6a
jy <nurja1218@gmail.com>
2019-04-04 01:14:14 +0900

    header.pug와 같이 login과 logout 상태에 따라 return값을 다르게 만듦(videoDetail도 미리 수정)

--- routes.js
+++ routes.js
@@ -19,9 +19,6 @@ const VIDEOS = "/videos";
 const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
-// ../videos/(video의 id)/edit
-// express가 :id를 보고 변하는 값임을 인식
-// (그냥 id는 text로 인식)
 const DELETE_VIDEO = "/:id/delete";
 
 const routes = {
@@ -31,12 +28,24 @@ const routes = {
     logout: LOGOUT,
     search: SEARCH,
     users: USERS,
-    userDetail: USER_DETAIL,
+    userDetail: id => {
+        if (id) {
+            return `/users/${id}`;
+        } else {
+            return USER_DETAIL;
+        }
+    },
     editProfile: EDIT_PROFILE,
     changePassword: CHANGE_PASSWORD,
     videos: VIDEOS,
     upload: UPLOAD,
-    videoDetail: VIDEO_DETAIL,
+    videoDetail: id => {
+        if (id) {
+            return `/videos/${id}`;
+        } else {
+            return VIDEO_DETAIL;
+        }
+    },
     editVideo: EDIT_VIDEO,
     deleteVideo: DELETE_VIDEO
 };
