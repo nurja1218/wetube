dfc04d15bfad706f4a31052211af14ec2df6bf74
jy <nurja1218@gmail.com>
2019-04-04 01:27:30 +0900

    home화면의 동영상을 클릭하면 videoDetail페이지로 이동

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,16 @@
 import express from "express";
 import routes from "../routes"
-import { upload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController";
+import { 
+    upload, 
+    videoDetail, 
+    editVideo, 
+    deleteVideo 
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
