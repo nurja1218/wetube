c945fc4385e3b262c7695f09966673b90be9b2f1
jy <nurja1218@gmail.com>
2019-04-04 01:10:09 +0900

    users 삭제

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,8 +26,11 @@ export const postLogin = (req, res) => {
     res.redirect(routes.home);
 }
 
-export const logout = (req, res) => res.render("logout", { pageTitle: "Log Out" });
-export const users = (req, res) => res.render("users", { pageTitle: "Users" });
-export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
-export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
-export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
+export const logout = (req, res) => 
+    res.render("logout", { pageTitle: "Log Out" });
+export const userDetail = (req, res) => 
+    res.render("userDetail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) => 
+    res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) => 
+    res.render("changePassword", { pageTitle: "Change Password" });
