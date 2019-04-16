acbdcc5f13a673af6ed1b7265584eebf2b59d73e
jy <nurja1218@gmail.com>
2019-04-17 05:56:34 +0900

    postJoin에서 join기능 수행을 위해 User.js를 import하고 등록하는 함수 코드 추가

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,10 @@
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   const {
     body: { name, email, password, password2 }
   } = req;
@@ -12,10 +13,17 @@ export const postJoin = (req, res) => {
     res.status(400);
     res.render("join", { pageTitle: "Join" });
   } else {
-    console.log(req.body);
-    // To Do : Register User
+    try {
+      const user = await User({
+        name,
+        email
+      });
+      await User.register(user, password);
+    } catch (error) {
+      console.log("error");
+      res.redirect(routes.home);
+    }
     // To Do : Log User In
-    res.redirect(routes.home);
   }
 };
 
