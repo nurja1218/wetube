86211c1f960c748fe44e31b5f0195fc03fcbb52c
jy <nurja1218@gmail.com>
2019-04-19 16:01:59 +0900

    github로그인 시도할 때의 정보를 받아올 링크 & 콜백 링크를 위해 코드 추가 수정

--- routes.js
+++ routes.js
@@ -21,6 +21,11 @@ const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
+// Github
+
+const GITHUB = "/auth/github";
+const GITHUB_CALLBACK = "/auth/github/callback";
+
 const routes = {
   home: HOME,
   join: JOIN,
@@ -47,15 +52,19 @@ const routes = {
   editVideo: id => {
     if (id) {
       return `/videos/${id}/edit`;
+    } else {
+      return EDIT_VIDEO;
     }
-    return EDIT_VIDEO;
   },
   deleteVideo: id => {
     if (id) {
       return `/videos/${id}/delete`;
+    } else {
+      return DELETE_VIDEO;
     }
-    return DELETE_VIDEO;
-  }
+  },
+  gitHub: GITHUB,
+  githubCallback: GITHUB_CALLBACK
 };
 
 export default routes;
