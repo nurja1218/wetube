4f501e40ec149314ed26b8af4cfe50a5e1cc3745
jy <nurja1218@gmail.com>
2019-05-09 15:37:36 +0900

    avatar에 대한 미들웨어 생성

--- middlewares.js
+++ middlewares.js
@@ -2,12 +2,12 @@ import multer from "multer";
 import routes from "./routes";
 
 const multerVideo = multer({ dest: "uploads/videos/" });
+const multerAvatar = multer({ dest: "uploads/avatars/" });
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
   res.locals.loggedUser = req.user || null;
-  console.log(req.user);
   next();
 };
 
@@ -28,3 +28,4 @@ export const onlyPrivate = (req, res, next) => {
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
+export const uploadAvatar = multerAvatar.single("avatar");
