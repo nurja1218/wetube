0b00b8f973693408823df9c186356a6d3cb37560
jy <nurja1218@gmail.com>
2019-05-09 21:18:17 +0900

    video 업로드시 작성자 추가 & 작성자(creator)정보를 객체로 받아오도록 코드 수정

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -24,6 +24,8 @@ export const search = async (req, res) => {
   res.render("search", { pageTitle: "Search", outcome, videos });
 };
 
+// Upload
+
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 
@@ -35,8 +37,11 @@ export const postUpload = async (req, res) => {
   const newVideo = await Video.create({
     fileUrl: path,
     title,
-    description
+    description,
+    creator: req.user.id
   });
+  req.user.videos.push(newVideo.id);
+  req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
@@ -45,7 +50,8 @@ export const videoDetail = async (req, res) => {
     params: { id }
   } = req;
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("creator");
+    console.log(video);
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     console.log(error);
