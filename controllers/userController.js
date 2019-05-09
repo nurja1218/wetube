40a07ee5505d3181efbf6c5b7ef96c9505ea4c1a
jy <nurja1218@gmail.com>
2019-05-09 15:39:10 +0900

    edit profile을 post방식으로 진행할때 (사진 변경) -> name,email은 그대로 가져와서 적용 / avatar는 변경할 경우 새로 update 변경 안할경우 request객체가 갖고 있는 값을 그대로 가져도록 코드 수정

--- controllers/userController.js
+++ controllers/userController.js
@@ -119,5 +119,24 @@ export const userDetail = async (req, res) => {
 
 export const getEditProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
+
+export const postEditProfile = async (req, res) => {
+  const {
+    body: { name, email },
+    file
+  } = req;
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      name,
+      email,
+      avatarUrl: file ? file.path : req.user.avatarUrl
+    });
+    res.redirect(routes.me);
+  } catch (error) {
+    res.render("editProfile", { pageTitle: "Edit Profile" });
+    console.log(error);
+  }
+};
+
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
