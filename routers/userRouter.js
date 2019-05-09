46abdbf7196c7f7b6c913c6f63e79a9baf57c1c2
jy <nurja1218@gmail.com>
2019-05-09 14:55:52 +0900

    editProfile함수명을 getEditProfile로 변경

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,14 +2,14 @@ import express from "express";
 import routes from "../routes";
 import {
   userDetail,
-  editProfile,
-  changePassword
+  changePassword,
+  getEditProfile
 } from "../controllers/userController";
 import { onlyPrivate } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.get(routes.userDetail(), onlyPrivate, userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
